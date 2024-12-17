4f51c49ee857a3ff06f9a69692d68827cbb38126
Isaac Opoku Nkansah <akwasi3g@gmail.com>
2024-12-17 06:16:54 +0000

    Update Term-Deposit.py

--- app/Term-Deposit.py
+++ app/Term-Deposit.py
@@ -158,3 +158,5 @@ if pred_button:
     prediction = "Yes" if duration > 200 else "No"
     st.write(f"Predicted Term Deposit Subscription: **{prediction}**")
 
+# Footer
+st.write("Developed with ❤️ by ISAAC OPOKU NKANSAH (ION)")
